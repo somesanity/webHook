f59d970f67daaeb9f9e87606ab7cc82325eb5f99
somesanity <gamesbaga2006@gmail.com>
2025-03-15 22:54:38 +0600

    add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const buttonIncrement = screen.getByText('+');
+  const buttonDecrement = screen.getByText('-');
+  
+  expect(buttonIncrement).toBeInTheDocument()
+  expect(buttonDecrement).toBeInTheDocument()
+  expect(screen.getByText('0')).toBeInTheDocument()
+  fireEvent.click(buttonIncrement);
+  expect(screen.getByText('1')).toBeInTheDocument()
+  fireEvent.click(buttonDecrement);
+  expect(screen.getByText('0')).toBeInTheDocument()
 });
